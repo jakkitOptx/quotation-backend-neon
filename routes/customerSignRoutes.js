f7133d7935261c06b0d3d463323677c0bb2bc54c
Arm <jakkit@optx.co.th>
2026-04-30 11:23:20 +0700

    Support signed customer e-signature API

--- routes/customerSignRoutes.js
+++ routes/customerSignRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const quotationController = require("../controllers/quotationController");
 
+router.get("/:token/signature-url", quotationController.getCustomerSignatureSignedUrl);
 router.post("/:token/accept", quotationController.acceptCustomerSignature);
 
 module.exports = router;
